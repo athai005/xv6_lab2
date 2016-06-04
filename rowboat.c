89a6d0d9e33b7e949db01e4d70ab47287911d08d
Alvin Thai <athai005@ucr.edu>
2016-06-03 20:58:40 -0700

    rowboat fixed

--- rowboat.c
+++ rowboat.c
@@ -12,16 +12,19 @@ int cann = 0;
 int numm = 0;
 int numc = 0;
 
+int numthreads = 0;
+int threadcount = 0;
+
 void rowboat()
 {
-   printf(1,"Rowboat sent\n");
+   printf(1,"Rowboat sent: ");
 }
 
 void MissionaryArrives(void* v)
 {
    sem_acquire(&l);
    miss++;
-   if(miss+cann<3 || (miss == 1 && cann == 2))
+   if(miss+cann<3 && threadcount < numthreads)
    {
       sem_signal(&l);
       sem_acquire(&m);
@@ -29,7 +32,12 @@ void MissionaryArrives(void* v)
    }
    else
    {
-      if (miss >= 3)
+      if (miss == 1 && cann == 2) 
+      {
+      	sem_signal(&l);
+      	texit();
+      }
+      else if (miss >= 3)
       {
          miss = miss - 3;
          sem_signal(&m);
@@ -48,10 +56,10 @@ void MissionaryArrives(void* v)
    			 rowboat();
 				printf(1, "Boat left with 2 Missionaries, 1 cannibal\n");
       }
-      else if (miss == 1 && cann == 2) 
+      else
       {
       	sem_signal(&l);
-      	exit();
+      	texit();
       }
    }
    texit();
@@ -61,21 +69,26 @@ void CannibalArrives(void* v)
 {
    sem_acquire(&l);
    cann++;
-   if(miss+cann<3 || (miss==1 && cann==2))
+   if(miss+cann<3 && threadcount < numthreads)
    {
       sem_signal(&l);
       sem_acquire(&c);
    }
    else
    {
-      if (cann >= 3) 
+      if (miss == 1 && cann == 2) 
+      {
+      	sem_signal(&l);
+      	texit();
+      }
+      else if (cann >= 3) 
       {
          cann = cann -3;
          sem_signal(&c);
          sem_signal(&c);
          sem_signal(&l);
          rowboat();
-				printf(1, "Boat left with 3 Missionaries\n");
+				printf(1, "Boat left with 3 Cannibals\n");
       }
       else if(miss>=2 && cann >= 1)
       {
@@ -87,10 +100,10 @@ void CannibalArrives(void* v)
          rowboat();
 				printf(1, "Boat left with 2 Missionaries, 1 cannibal\n");
       }
-      else if (miss == 1 && cann == 2) 
+      else
       {
       	sem_signal(&l);
-      	exit();
+      	texit();
       }
    }
    texit();
@@ -116,13 +129,11 @@ int main(int argc, char* argv[])
 	{
 		exit();
 	}
-	/*
-	if ( (numm + numc)%3 != 0)
+	if ( numm%3 == 1 && numc%3 == 2)
 	{
 		numm = numm - (numm%3);
 		numc = numc - (numc%3);
 	}
-	*/
 	//else if (numm%3 == 1 && numc%3 == 2) exit();
 
    Sem_init(&l, 1);
@@ -130,16 +141,41 @@ int main(int argc, char* argv[])
    Sem_init(&c, 0);
 
 	int i = 0;
+  numthreads = numm + numc;	
+  threadcount = 0;
+	void * slist[numthreads];
+	for(i=0; i < numthreads; i++)	
+	{
+		slist[i] = 0;
+	}
+
 	for (i = 0; i < numm; i++)
 	{
-		thread_create(MissionaryArrives, (void*)0);
+		void * tidm = thread_create(MissionaryArrives, (void*)0);
+		slist[threadcount] = tidm;
+		threadcount++;
 	}
 	for (i = 0; i < numc; i ++)
 	{
-		thread_create(CannibalArrives, (void*)0);
+		void * tidc = thread_create(CannibalArrives, (void*)0);
+		slist[threadcount] = tidc;
+		threadcount++;
 	}
 
-	while(wait() >= 0);
+	while(wait() >= 0) 
+	{
+		if ((miss == 1 && cann == 2)) break;
+		if ((miss + cann)%3 != 0 && threadcount != numthreads) break;
+	}
+
+	for (i = 0; i < numthreads ; i++)
+	{
+		if (slist[i] != 0)
+		{
+			void * f = slist[i];
+			free(f); 
+	  }
+	}
 
    exit();
 
